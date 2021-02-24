9af881a017e1d5e91a48f1a36b7de181b33a7c4d
Dean Spooner <dean.spooner@me.com>
2021-02-24 00:17:14 +0000

    Added contact and about pages, edited the home page, added javascript to buttons for background colours when highlighted.

---
+++ javascript.js
@@ -0,0 +1,23 @@
+var toplink = document.getElementsByClassName('toplink');
+
+      for (let i = 0; i < toplink.length; i++) {
+        toplink[i].addEventListener("mouseenter", function() {
+          this.classList.add('link-select');
+        });
+        toplink[i].addEventListener("mouseleave", function() {
+          this.classList.remove('link-select');
+        })
+      }
+
+var box = document.getElementsByClassName('box');
+
+      for (let i = 0; i < box.length; i++) {
+        box[i].addEventListener("mouseenter", function() {
+          this.classList.add('box-select');
+        });
+        box[i].addEventListener("mouseleave", function() {
+          this.classList.remove('box-select');
+        })
+      }
+
+      
